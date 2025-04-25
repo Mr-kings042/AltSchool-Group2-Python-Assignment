15528f97d564cbfbdd55596cfc5e6ee120903c39
Mr-kings042 <okohkings042@gmail.com>
2025-04-25 09:17:09 +0100

    ALT/SOE/024/4890, created the boilerplate for the project

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
+from fastapi import FastAPI,status,HTTPException, UploadFile, Form
+from uuid import UUID
+from models import Student, Teacher, Assignment
+from database import student_db, teacher_db
 
+app = FastAPI()
--- models.py
+++ models.py
@@ -1,1 +1,20 @@
+from pydantic import BaseModel
+from typing import Optional, List, Annotated
 
+
+class Student(BaseModel):
+    name: str
+    email: str
+
+
+class Teacher(BaseModel):
+    name: str
+    email: str
+
+class Assignment (BaseModel):
+    id: int
+    student_name: str
+    subject: str
+    description: str
+    filename: str
+    comments: List[str] = []
