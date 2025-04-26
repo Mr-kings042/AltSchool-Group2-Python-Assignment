e45e38d01f596ab1c56c5faa8ac216573c0fff6f
Mr-kings042 <okohkings042@gmail.com>
2025-04-26 20:46:39 +0100

    ALT/SOE/024/4890, worked on the  /assignments/{assignment_id}/comment endpoint

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI,status,HTTPException, UploadFile, Form
 from uuid import UUID
+from typing import Annotated
 from models import Student, Teacher, Assignment
-from database import student_db, teacher_db
+from database import student_db, teacher_db, assignment_db
 
 app = FastAPI()
+
+@app.post("/assignments/{assignment_id}/comment")
+def add_comment(assignment_id: int, 
+                teacher_name: Annotated[str, Form()],
+                comment: Annotated[str, Form()]):
+    assignment = assignment_db.get(str(assignment_id))
+    if not assignment:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Assignment not found")
+    Comment = f"{teacher_name}:{comment}"
+    assignment.comments.append(Comment)
+    return {"message": "Comment added successfully"}
