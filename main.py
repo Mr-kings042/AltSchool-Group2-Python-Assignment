8057d08dcf2c8551bd357e5c892158cdd28398f2
Mr-kings042 <okohkings042@gmail.com>
2025-04-26 19:53:27 +0100

    ALT/SOE/024/4890, Added some imports, i missed to the main.py file

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI,status,HTTPException, UploadFile, Form
 from uuid import UUID
+from typing import Annotated
 from models import Student, Teacher, Assignment
-from database import student_db, teacher_db
+from database import student_db, teacher_db,assignment_db
 
 app = FastAPI()
+
+@app.get("/")
+def Home():
+    return {"message": "Welcome to the Assignment API"}
